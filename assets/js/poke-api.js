5d6c3f0071ecde5c49fed42228faf1078a2b9ece
fsdalpiaz <filipsdalpiaz@gmail.com>
2023-01-13 17:36:01 -0300

    Upgrade

---
+++ assets/js/poke-api.js
@@ -0,0 +1,15 @@
+
+
+const pokeApi = {}
+
+pokeApi.getPokemons = (offset = 0, limit = 10) {
+  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+  return fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .catch((error) => console.error(error));
+}
+
+//função sintaxe reduzida,
+//sem contexto, usado quando tem callback ou
+//contexto isolado, não precisa declarar o corpo da função
