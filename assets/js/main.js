675f22b194b45ac6c8feabcc3f63361e35c156d4
fsdalpiaz <filipsdalpiaz@gmail.com>
2023-01-13 01:09:45 -0300

    add fetch, response manipulation

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,25 @@ const offset = 0
 const limit = 10
 const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
 
-fetch(url).then(function (response){
-  console.log(response);
-});
+//função sintaxe reduzida,
+//sem contexto, usado quando tem callback ou
+//contexto isolado, não precisa declarar o corpo da função
 
+fetch(url)
+  .then((response) => response.json())
+  .then((jsonBody) => console.log(jsonBody))
+  .catch((error) => console.error(error));
+
+// fetch(url)
+//   .then(function (response) { //função anônima
+//     return response.json()
+//   })
+//   .then(function (jsonBody) {
+//       console.log(jsonBody);
+//   })
+//   .catch(function (error) {
+//     console.error(error);
+//   })
+//   .finally(function () {
+//     console.log("Requisição concluída!");
+//   })
