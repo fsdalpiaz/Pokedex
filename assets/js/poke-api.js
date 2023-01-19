b9a7bdf137ff5609aecc74b1d6926b5efc204e33
fsdalpiaz <filipsdalpiaz@gmail.com>
2023-01-19 00:00:15 -0300

    Improve more HTML manipulation

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -2,13 +2,30 @@
 
 const pokeApi = {}
 
+pokeApi.getPokemonDetails = (pokemon) => {
+  return fetch(pokemon.url).then((response) => response.json())
+}
+
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+  
   return fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
-    .catch((error) => console.error(error));
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetails))
+    .then((detailRequests) => Promise.all(detailRequests))
+    .then((pokemonsDetails) => pokemonsDetails)
 }
+
+
+
+
+
+
+
+
+
+
 
 //função sintaxe reduzida,
 //sem contexto, usado quando tem callback ou
